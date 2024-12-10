b34c0bb2562ee8e27fbcf62ed2f041578b572431
Maulid Fajar <67719670+MaulidFajar@users.noreply.github.com>
2024-12-10 17:55:06 +0700

    styling mobile view

--- src/main.js
+++ src/main.js
@@ -1,17 +1,35 @@
 const burgerBtn = document.querySelector(".burger_btn");
 const navbarList = document.querySelector(".navbar_list");
 const main = document.getElementById("main");
+const heroImg = document.querySelector(".hero_img");
 
 burgerBtn.addEventListener("click", () => {
   burgerBtn.classList.toggle("nav_active");
+  overlay.classList.toggle("opacity-50");
+  overlay.classList.toggle("pointer-events-auto");
+  navbarList.classList.toggle("w-64");
 
-  if (navbarList.classList.contains("w-0")) {
-    navbarList.classList.remove("w-0");
-    navbarList.classList.add("w-64");
-    document.body.classList.add("bg-black/55");
+  if (burgerBtn.classList.contains("nav_active")) {
+    burgerBtn.classList.remove("absolute");
+    burgerBtn.classList.add("fixed");
+    burgerBtn.classList.add("right-5");
   } else {
-    navbarList.classList.add("w-0");
-    navbarList.classList.remove("w-64");
-    document.body.classList.remove("bg-black/55");
+    burgerBtn.classList.add("absolute");
+    burgerBtn.classList.remove("fixed");
+    burgerBtn.classList.remove("right-5");
+    burgerBtn.classList.add("right-1");
   }
 });
+
+const responsiveImage = () => {
+  const screenSize = window.innerWidth;
+
+  if (screenSize <= 718) {
+    heroImg.src = "assets/images/image-web-3-mobile.jpg";
+  } else {
+    heroImg.src = "assets/images/image-web-3-desktop.jpg";
+  }
+};
+
+window.addEventListener("resize", responsiveImage);
+window.addEventListener("load", responsiveImage);
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,33 @@ module.exports = {
   theme: {
     container: {
       center: true,
+      padding: "1rem",
       screens: {
         md: "1080px",
       },
     },
     extend: {
+      textColor: {
+        "soft-orange": "hsl(35, 77%, 62%)",
+        "soft-red": "hsl(5, 85%, 63%)",
+        "off-white": "hsl(36, 100%, 99%)",
+        "grayish-blue": "hsl(233, 8%, 79%)",
+        "dark-grayish-blue": "hsl(236, 13%, 42%)",
+        "dark-blue": "hsl(240, 100%, 5%)",
+      },
+      backgroundColor: {
+        "soft-red": "hsl(5, 85%, 63%)",
+        "dark-blue": "hsl(240, 100%, 5%)",
+      },
       fontFamily: {
         primary: ["Inter", "sans-serif"],
+      },
+      letterSpacing: {
+        "super-wide": "4px",
+      },
+      fontSize: {
+        sm: "15px",
+        "4xl": ["2.5rem", "1"],
       },
     },
   },
